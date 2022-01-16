c28a2b41573dd8778e88a68401f0c984b2c49060
Manuel <manuel@Mac-mini-de-Manuel.local>
2022-01-16 19:25:13 +0100

    Add Net 6 iOS template

---
+++ Net6iOSTemplate/AppDelegate.fs
@@ -0,0 +1,22 @@
+﻿namespace Net6iOSTemplate
+
+open System
+
+open UIKit
+open Foundation
+
+[<Register(nameof AppDelegate)>]
+type AppDelegate() =
+    inherit UIApplicationDelegate()
+       
+    override val Window = null with get, set
+
+    override this.FinishedLaunching(_, _) =
+        this.Window <- new UIWindow(UIScreen.MainScreen.Bounds)
+        let vc = new UIViewController()
+        vc.View.BackgroundColor <- UIColor.Red
+        this.Window.RootViewController <- vc
+        this.Window.MakeKeyAndVisible()
+        true
+        
+       
---
+++ Net6iOSTemplate/Library.fs
@@ -0,0 +1,6 @@
+﻿namespace Net6iOSTemplate
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
+
